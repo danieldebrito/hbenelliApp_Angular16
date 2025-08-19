d623a21dd29028264b172252bfe8a04c65b78be8
Daniel de Brito <danieldebrito@outlook.com>
2025-08-19 00:50:47 -0300

    build

--- src/app/home/components/carousel/carousel.component.ts
+++ src/app/home/components/carousel/carousel.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent {
+  constructor(private router: Router) {}
 
+  navegar(arg0: string) {
+    this.router.navigate([arg0]);
+  }
 }
